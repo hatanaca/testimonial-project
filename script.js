2ab08da2249b1fcd98ada4bd0eb9ab628e1f3170
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-06 13:17:39 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('Arquivo Javascript carregado com sucesso!')
+const testimonials = [
+	{
+		text:"textoGenerico",
+		author:"textoGenerico"
+	},
+	{
+		text:"textoGenerico" ,
+		author:"textoGenerico" 
+	},
+	{
+		text:"textoGenerico" ,
+		author:"textoGenerico" 
+	}
+];
+
+let currentIndex = 0;
+const textElement = document.getElementById("testimonial-text");
+const authorElement = document.getElementById("testimonial-author");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+function updateTestimonial(index) {
+	textElement.textContent = testimonials[index].text;
+	authorElement.textContent = testimonials[index].author;
+}
+
+prevBtn.addEventListener("click", () => {
+	currentIndex = (currentIndex === 0) ? testimonials.length - 1 : currentIndex - 1;
+	updateTestimonial(currentIndex);
+});
+
+nextBtn.addEventListener("click", () => {
+	currentIndex = (currentIndex === testimonials.length -1) ? 0 :currentIndex +1;
+	updateTestimonial(currentIndex);
+});
+
+		
